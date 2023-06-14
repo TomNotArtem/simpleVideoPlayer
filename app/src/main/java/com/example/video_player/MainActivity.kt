631951263997fb7415cc6a44cc4a59e4722866b1
ArtemBlazh <ablazhievskii@gmail.com>
2023-06-14 19:52:34 +0800

    Added videoView

--- app/src/main/java/com/example/video_player/MainActivity.kt
+++ app/src/main/java/com/example/video_player/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.video_player
 
 import android.net.Uri
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.video_player.databinding.ActivityMainBinding
@@ -17,6 +19,8 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val videoView = findViewById<VideoView>(binding.testView.id)
 
         val uri: Uri = Uri.parse("android.resource://" + packageName + "/raw/test")
     }
